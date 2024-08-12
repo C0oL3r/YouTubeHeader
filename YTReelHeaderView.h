fbbbae7cceb5a1c26ce2ec74f4af4db3bc370eeb
C0oL3r <35148210+C0oL3r@users.noreply.github.com>
2024-08-12 03:52:11 +0300

    Create YTReelHeaderView.h

---
+++ YTReelHeaderView.h
@@ -0,0 +1,21 @@
+#import <UIKit/UIKit.h>
+#import "YTReelPlayerViewController.h"
+#import "YTReelTransparentStackView.h"
+#import "YTReelPlayerButton.h"
+#import "YTQTMButton.h"
+
+@interface YTReelHeaderView : UIView
+@property (nonatomic, strong) id<YTReelHeaderDelegate> delegate;
+@property (nonatomic, strong) YTReelTransparentStackView *leftStackView;
+@property (nonatomic, strong) YTReelTransparentStackView *rightStackView;
+@property (nonatomic, strong) YTQTMButton *qualityButton;
+- (void)addView:(id)view toStackView:(id)stackView withSize:(CGSize)size;
+- (void)addLeftStackView:(id)view toView:(id)parentView;
+- (void)addRightStackView:(id)view toView:(id)parentView;
+- (void)setRightNavigationButtonsArray:(id)buttonsArray;
+- (id<YTReelHeaderDelegate>)delegate;
+- (void)setDelegate:(id<YTReelHeaderDelegate>)delegate;
+- (void)didPressVideoQuality:(id)arg;
+- (YTQTMButton *)buttonWithImage:(UIImage *)image accessibilityLabel:(NSString *)accessibilityLabel verticalContentPadding:(CGFloat)verticalContentPadding;
+- (YTQTMButton *)createButton:(NSString *)tweakId accessibilityLabel:(NSString *)accessibilityLabel selector:(SEL)selector;
+@end
